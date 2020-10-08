1e262c9536191e754703910b7359a1cb653fed34
Andras Schaffer <eosbandi@gmail.com>
2020-10-08 16:10:14 +0200

    First release version

--- RACConfigName.cs
+++ RACConfigName.cs
@@ -1,17 +1,19 @@
-﻿using MissionPlanner.Utilities;
+﻿// Mission Planner plugin for checking config file name and add description for the given config
+//
+// Don't do anything if the config filename is config.xml (This is the default configuration)
+
+using MissionPlanner.Controls;
+using MissionPlanner.Utilities;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Windows.Forms;
-using System.Diagnostics;
-using MissionPlanner.Controls.PreFlight;
-using MissionPlanner.Controls;
-using System.Linq;
 
 namespace MissionPlanner.RACConfigName
 {
     public class RACConfigNamePlugin : MissionPlanner.Plugin.Plugin
     {
+        private string MPConfigDescription;
+
         public override string Name
         {
             get { return "RACConfigName"; }
@@ -19,35 +21,80 @@ namespace MissionPlanner.RACConfigName
 
         public override string Version
         {
-            get { return "0.1"; }
+            get { return "1.0"; }
         }
 
         public override string Author
         {
-            get { return "Andras Schaffer"; }
+            get { return "Andras Schaffer (EOSBandi)"; }
         }
 
         //[DebuggerHidden]
         public override bool Init()
         {
-            loopratehz = 1;  //Every 
-
+            loopratehz = 0; //we do not use the loop
             return true;
         }
 
         public override bool Loaded()
         {
+            //Check if we have other config than config.xml
+            if (String.Compare(Settings.FileName, "config.xml", true) != 0)
+            {
+                //Hook kwyboard command for change description
+                MainV2.instance.ProcessCmdKeyCallback += this.Instance_ProcessCmdKeyCallback;
+
+                MPConfigDescription = Host.config["MPConfigDesc", ""];
+                if (MPConfigDescription.Length == 0)    //opps no description yet
+                    GetDescription();
+                UpdateTitleBar();
+            }
+
             return true;
         }
 
         public override bool Loop()
         {
-
             return true;
         }
 
         public override bool Exit()
         {
+            return true;
+        }
+
+        private void GetDescription()
+        {
+            var result = InputBox.Show("Description for Config file : " + Settings.FileName, "Enter description", ref MPConfigDescription);
+            if (result != DialogResult.Cancel)
+            {
+                Host.config["MPConfigDesc"] = MPConfigDescription;
+            }
+        }
+
+        private void UpdateTitleBar()
+        {
+            string strVersion = File.Exists("version.txt")
+                                 ? File.ReadAllText("version.txt")
+                                 : System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            string MPversion = "MP " + Application.ProductVersion + " build " + strVersion;
+            if (MPConfigDescription.Length > 0)
+                MainV2.titlebar = MPversion + " (" + MPConfigDescription + ") ";
+            else
+                MainV2.titlebar = MPversion;
+
+            MainV2.instance.Text = MainV2.titlebar + Host.comPort.MAV?.VersionString; //Do not add FW verion to titlebar, unless it duplicated at reconnect
+        }
+
+        private bool Instance_ProcessCmdKeyCallback(ref System.Windows.Forms.Message msg, System.Windows.Forms.Keys keyData)
+        {
+            //Add our shortcut
+
+            if (keyData == (Keys.Alt | Keys.D))
+            {
+                GetDescription();
+                UpdateTitleBar();
+            }
             return true;
         }
     }
